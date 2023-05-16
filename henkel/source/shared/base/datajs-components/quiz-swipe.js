c307617b8aa48cd75533f81b39a4bedf5d097e99
szangyi <alexandra.mszrs@gmail.com>
2023-05-16 13:59:25 +0200

    data js finished ready for built

--- henkel/source/shared/base/datajs-components/quiz-swipe.js
+++ henkel/source/shared/base/datajs-components/quiz-swipe.js
@@ -1,7 +1,17 @@
-// Add Lottie-player libraries to <head> index.html
-// <script src="lottieplayer-min.js"></script>
-// <script src="lottie-interactivity-min.js"></script>
-// <script src="bodymovin-min.js"></script>
+// -----------------------------------------------------------------------------
+// Quiz Swipe Component Javascript
+//
+// Note!
+// With this script you can control swipe quiz cards
+// followed by the swipe-answer.
+//
+//
+// Markup example:
+// <div data-comp="card-container stack">
+//      <div data-comp="card rounded swipe" data-quiz="swipe"></div>
+// </div>
+// -----------------------------------------------------------------------------
+
 
 function _all(q, e = document) {
     return [...e.querySelectorAll(q)]
@@ -12,79 +22,96 @@ function _one(q, e = document) {
 }
 
 
-// var cards = _all("[data-comp*='card ']");
+// Variables
+var cardStackElements = _all('[data-comp*="card-container"]>*')
 var cards = _all("[data-comp*='card']:not([data-comp*='-'])");
+var btns = _all("[data-comp*='button']");
 var swipecards = _all("[data-comp*='swipe']");
-
-let questionNumber = 0 // quiz state
-var pagination = _one('.pagination')
-var swipecardslength = swipecards.length
+var overlays = _all("[data-comp*='overlay']");
+var pagination = _one('.pagination');
+let questionNumber = 1;
+let cardNumber = -1;
 
 
 /* ---------------- */
-/* Question numbers */
+/* Initial          */
 /* ---------------- */
-
-setNr()
-
-setListenerForButtons()
-addListeners()
-
-
-// console.log({ questionNumber })
-
-function setNr() {
-    questionNumber++;
-    console.log({ questionNumber })
-
-    if (pagination) {
-        pagination.innerHTML = `${questionNumber}/${swipecardslength}`
-    }
+function initial() {
+    stackCards();
+    setNr(); // + setActiveCard();
+    setListeners();
 }
 
+initial()
 
-// initialized at start of each card
-function setListenerForButtons() {
-    cards[questionNumber - 1].classList.add('active')
+function stackCards() {
+    // Card stacking
+    cardStackElements.forEach((element, index) => {
+        element.style.setProperty("--z", cardStackElements.length - index);
+    });
 }
 
-
-/* ---- */
-/* Quiz */
-/* ---- */
-
-function addListeners() {
-
-    cards.forEach((card, index, cards) => {
-        const btnSubmit = card.querySelector('button.submit')
+function setListeners() {
+    cards.forEach(card => {
         const btnNext = card.querySelector('button.next')
         let swipe = false
         let startX = 0
         let pct = 0
 
-        /* ------- Swipe ------- */
+        card.addEventListener('click', swipeclick)
 
-        card.addEventListener('pointerdown', swipeStart)
-        card.addEventListener('click', swipeclick())
+        if (!card.classList.contains('swipe-answer')) {
+            card.addEventListener('pointerdown', swipeStart)
+            card.addEventListener('pointermove', swipeMove)
+            card.addEventListener('pointerup', swipeEnd)
+        }
 
-        addEventListener('pointermove', swipeMove)
-        addEventListener('pointerup', swipeEnd)
+        function swipeclick(e) {
+            if (swipe) return
+
+            // return function curried_func(e) {
+            const card = e.currentTarget
+            const target = e.target
+
+            if (target != swipe && target.dataset.direction) {
+                //  enable swipe 
+                swipe = card
+
+                // if item (button) has data-direction attribute 
+                // set percentage based on value    
+                if (target.dataset.direction == "left") {
+                    pct = -1;
+                    swipe.dataset.correct = true
+                } else if (target.dataset.direction == "right") {
+                    pct = 1;
+                    swipe.dataset.correct = false
+                }
+
+                // update percentage variable for css
+                swipe.style.setProperty('--swipe-pct', Math.abs(pct) / pct * 2)
+
+                swipe = false
+
+                answerSubmit(card)
+
+            }
+
+            // }
+
+        }
 
         function swipeStart(e) {
             swipe = e.currentTarget
             startX = e.clientX
             swipe.style.setProperty('--transition', 0)
-
         }
 
         function swipeMove(e) {
             if (!swipe) return
-
             let deltaX = e.clientX - startX
             let rect = swipe.getBoundingClientRect()
             let width = rect.width
             pct = deltaX / width
-
             swipe.style.setProperty('--swipe-pct', pct)
             swipe.style.setProperty('--opacity', Math.abs(pct) * 3)
             swipe.dataset.direction = pct > 0 ?
@@ -98,100 +125,76 @@ function addListeners() {
 
             swipe.style.removeProperty('--transition')
 
+            /* incomplete swipe */
             if (Math.abs(pct) < .1) {
-                /* incomplete swipe */
                 swipe.style.removeProperty('--swipe-pct')
                 swipe.style.removeProperty('--opacity')
-            } else {
-                /* complete swipe */
-                swipe.style.setProperty('--swipe-pct', Math.abs(pct) / pct * 2)
+            }
+            /* complete swipe */
+            else {
+                card.style.setProperty('--swipe-pct', Math.abs(pct) / pct * 2)
 
-                // swipeAnswerCheck(e) function moved here
                 if ((swipe.dataset.correct == "left" && pct < 0) || (swipe.dataset.correct == "right" && pct > 0)) {
-                    // console.log('right')
                     swipe.dataset.correct = true
                 } else {
-                    // console.log('wrooong')
                     swipe.dataset.correct = false
                 }
-
-                // add discarded class to swiped card so the answer will show up
-                card.classList.add('discarded')
-
+                answerSubmit(card)
             }
 
-            swipe = false
-            setListenerForButtons();
+            swipe = false         // disable swipe to avoid swipe move 
         }
-
-        function swipeclick() {
-            console.log('this is a swipeclick')
-            if (swipe) return
-
-            return function curried_func(e) {
-                console.log("calling click function");
-                const card = e.currentTarget
-                const target = e.target
-                console.log({target})
-
-
-                if (target != swipe && target.dataset.direction) {
-                    //  enable swipe 
-                    swipe = card
-
-                    // if item (button) has data-direction attribute 
-                    // set percentage based on value                 
-                    if (target.dataset.direction == "left") {
-                        pct = -1;
-                        swipe.dataset.correct = true
-                    }
-                    if (target.dataset.direction == "right") {
-                        pct = 1;
-                        swipe.dataset.correct = false
-                    }
-
-                    // update percentage variable for css
-                    swipe.style.setProperty('--swipe-pct', Math.abs(pct) / pct * 2)
-
-                    card.classList.add('discarded')
-                    // setLottie()
-
-                    // disable swipe to avoid swipe move 
-                    swipe = false
-
-
-                }
-
-            }
-
-            setListenerForButtons();
-        }
-
-
-        /* ------- Global ------- */
 
         if (btnNext) {
             btnNext.addEventListener('click', e => {
-                card.classList.add('discarded');
-                // _all('.character-image-slidein')[questionNumber - 1].classList.add('discarded')
-                const target = e.target
-                console.log('nextbuttoncliceeeeked')
-
-                setNr();
-                setListenerForButtons();
-
-                // setLottie();
-                // setLottieDesktop(questionNumber)
+                nextQuestion(e)
             })
         }
     })
 }
 
 
-/* ------------- */
-/* Card stacking */
-/* ------------- */
 
-cards.forEach((card, index, cards) => {
-    card.style.setProperty("--z", cards.length - index);
-});
+
+/* ---------------- */
+/* Answer Submit    */
+/* ---------------- */
+function answerSubmit(card) {
+    setNr(card);
+}
+
+
+
+/* ---------------- */
+/* Next Question    */
+/* ---------------- */
+function nextQuestion(e) {
+    const card = e.target
+    questionNumber++
+    setNr();
+}
+
+/* ---------------- */
+/* Change number    */
+/* ---------------- */
+
+
+function setNr() {
+    cardNumber++;
+    setActiveCard()
+
+    if (pagination) {
+        pagination.innerHTML = `${questionNumber}/${swipecards.length}`
+    }
+}
+
+function setActiveCard() {
+    let card = cards[cardNumber]
+
+    if (cards[cardNumber - 1]) {
+        cards[cardNumber - 1].classList.remove('active')
+        cards[cardNumber - 1].classList.add('discarded')
+    }
+
+    card.classList.add('active')
+}
